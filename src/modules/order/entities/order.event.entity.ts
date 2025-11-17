fa36410f29051085b64643f581e20f4e99f16a74
chenruogeng <chenruogeng@126.com>
2025-11-17 14:59:42 +0800

    补充事件表实体

---
+++ src/modules/order/entities/order.event.entity.ts
@@ -0,0 +1,161 @@
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  Index,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({
+  name: 'order_event',
+  engine: 'InnoDB',
+  comment: '事件表',
+  database: 'seaways-base-business-center',
+  synchronize: false,
+})
+@Index('idx_business_id', ['businessId'])
+@Index('idx_event_type', ['eventType'])
+@Index('idx_created_time_event_status', ['createdTime', 'eventStatus'])
+@Index('idx_event_status', ['eventStatus'])
+export class OrderEvent {
+  @PrimaryColumn({ type: 'bigint', comment: '事件ID 雪花算法生成' })
+  id: string;
+
+  @Column({
+    name: 'event_type',
+    type: 'varchar',
+    length: 64,
+    comment: '事件类型，如：ORDER_PUSH',
+  })
+  eventType: string;
+
+  @Column({
+    name: 'event_app_id',
+    type: 'varchar',
+    length: 128,
+    nullable: true,
+    comment: '触发事件的应用ID',
+  })
+  eventAppId?: string;
+
+  @Column({
+    name: 'event_app_name',
+    type: 'varchar',
+    length: 128,
+    nullable: true,
+    comment: '触发事件的应用名称',
+  })
+  eventAppName?: string;
+
+  @Column({
+    name: 'event_status',
+    type: 'tinyint',
+    width: 1,
+    default: 0,
+    comment: '事件处理状态，0-未开始，1-已完成，2-错误',
+  })
+  eventStatus: number;
+
+  @Column({
+    name: 'event_message',
+    type: 'varchar',
+    length: 1024,
+    nullable: true,
+    comment: '事件处理信息',
+  })
+  eventMessage?: string;
+
+  @Column({
+    name: 'business_id',
+    type: 'bigint',
+    comment: '业务ID, 对应订单ID',
+  })
+  businessId: string;
+
+  @Column({
+    name: 'business_title',
+    type: 'varchar',
+    length: 256,
+    nullable: true,
+    comment: '业务实例标题',
+  })
+  businessTitle?: string;
+
+  @Column({
+    type: 'json',
+    nullable: false,
+    default: () => 'JSON_OBJECT()',
+    comment: '业务实例详情信息',
+  })
+  details: object;
+
+  @Column({
+    name: 'business_status',
+    type: 'varchar',
+    length: 32,
+    comment: '业务实例的状态',
+  })
+  businessStatus: string;
+
+  @Column({
+    name: 'business_message',
+    type: 'varchar',
+    length: 1024,
+    nullable: true,
+    comment: '业务实例处理信息',
+  })
+  businessMessage?: string;
+
+  @Column({
+    type: 'varchar',
+    length: 10,
+    default: 'NO',
+    comment: '是否删除，YES-删除，NO-未删除',
+  })
+  deleted: string;
+
+  @CreateDateColumn({
+    name: 'created_time',
+    type: 'datetime',
+    precision: 0,
+    default: () => 'CURRENT_TIMESTAMP',
+    comment: '创建时间(收到事件时间)',
+  })
+  createdTime: Date;
+
+  @Column({
+    name: 'reviser_id',
+    type: 'bigint',
+    nullable: true,
+    comment: '修改人ID',
+  })
+  reviserId?: string;
+
+  @UpdateDateColumn({
+    name: 'revised_time',
+    type: 'datetime',
+    precision: 0,
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+    comment: '修改时间',
+  })
+  revisedTime: Date;
+
+  @Column({
+    name: 'reviser_name',
+    type: 'varchar',
+    length: 255,
+    nullable: true,
+    comment: '更新人名字',
+  })
+  reviserName?: string;
+
+  @Column({
+    name: 'last_operate_program',
+    type: 'varchar',
+    length: 128,
+    comment: '最后操作的程序',
+  })
+  lastOperateProgram: string;
+}
--- src/modules/order/entities/order.main.entity.ts
+++ src/modules/order/entities/order.main.entity.ts
@@ -1,352 +1,358 @@
-import { Column, Entity, Index } from "typeorm";
+import { Column, Entity, Index } from 'typeorm';
 
-@Index("uniq_order_code", ["orderCode"], { unique: true })
-@Index("idx_online_order_code", ["onlineOrderCode"], {})
-@Index("idx_ori_inner_order_code", ["oriInnerOrderCode"], {})
-@Index("idx_customer_time", ["customerId", "createdTime"], {})
-@Index("idx_order_status", ["orderStatus"], {})
-@Index("idx_created_time", ["createdTime"], {})
-@Entity("order_main", { schema: "seaways_base_business_center" })
+@Index('uniq_order_code', ['orderCode'], { unique: true })
+@Index('idx_online_order_code', ['onlineOrderCode'], {})
+@Index('idx_ori_inner_order_code', ['oriInnerOrderCode'], {})
+@Index('idx_customer_time', ['customerId', 'createdTime'], {})
+@Index('idx_order_status', ['orderStatus'], {})
+@Index('idx_created_time', ['createdTime'], {})
+@Entity({
+  name: 'order_main',
+  engine: 'InnoDB',
+  comment: '订单主表',
+  database: 'seaways-base-business-center',
+  synchronize: false,
+})
 export class OrderMainEntity {
-  @Column("bigint", {
+  @Column('bigint', {
     primary: true,
-    name: "id",
-    comment: "订单ID 雪花算法生成",
+    name: 'id',
+    comment: '订单ID 雪花算法生成',
   })
   id: string;
 
-  @Column("varchar", {
-    name: "order_code",
+  @Column('varchar', {
+    name: 'order_code',
     unique: true,
     comment: '订单编号, 全局唯一，格式："XX" + YYYYMMDD + NNNNNN',
     length: 128,
   })
   orderCode: string;
 
-  @Column("varchar", {
-    name: "online_order_code",
+  @Column('varchar', {
+    name: 'online_order_code',
     nullable: true,
     comment: '线上订单号, 格式: "CCCCC"(业务代码) + YYYYMMDD + NNN',
     length: 128,
   })
   onlineOrderCode: string | null;
 
-  @Column("bigint", {
-    name: "ori_inner_order_code",
+  @Column('bigint', {
+    name: 'ori_inner_order_code',
     nullable: true,
-    comment: "原内部单号(聚水潭内部单号)",
+    comment: '原内部单号(聚水潭内部单号)',
   })
   oriInnerOrderCode: string | null;
 
-  @Column("varchar", { name: "order_status", comment: "订单状态", length: 32 })
+  @Column('varchar', { name: 'order_status', comment: '订单状态', length: 32 })
   orderStatus: string;
 
-  @Column("varchar", {
-    name: "cancelled_message",
+  @Column('varchar', {
+    name: 'cancelled_message',
     nullable: true,
-    comment: "订单取消原因",
+    comment: '订单取消原因',
     length: 128,
   })
   cancelledMessage: string | null;
 
-  @Column("datetime", {
-    name: "audit_time",
+  @Column('datetime', {
+    name: 'audit_time',
     nullable: true,
-    comment: "订单审核通过时间",
+    comment: '订单审核通过时间',
   })
   auditTime: Date | null;
 
-  @Column("datetime", {
-    name: "push_time",
+  @Column('datetime', {
+    name: 'push_time',
     nullable: true,
-    comment: "订单推送时间",
+    comment: '订单推送时间',
   })
   pushTime: Date | null;
 
-  @Column("datetime", {
-    name: "delivery_time",
+  @Column('datetime', {
+    name: 'delivery_time',
     nullable: true,
-    comment: "订单发货时间",
+    comment: '订单发货时间',
   })
   deliveryTime: Date | null;
 
-  @Column("bigint", { name: "customer_id", comment: "客户ID" })
+  @Column('bigint', { name: 'customer_id', comment: '客户ID' })
   customerId: string;
 
-  @Column("varchar", {
-    name: "customer_name",
-    comment: "客户名称",
+  @Column('varchar', {
+    name: 'customer_name',
+    comment: '客户名称',
     length: 128,
   })
   customerName: string;
 
-  @Column("varchar", {
-    name: "customer_jst_id",
+  @Column('varchar', {
+    name: 'customer_jst_id',
     nullable: true,
-    comment: "客户聚水潭ID",
+    comment: '客户聚水潭ID',
     length: 24,
   })
   customerJstId: string | null;
 
-  @Column("bigint", {
-    name: "regional_head_id",
+  @Column('bigint', {
+    name: 'regional_head_id',
     nullable: true,
-    comment: "大区负责人ID",
+    comment: '大区负责人ID',
   })
   regionalHeadId: string | null;
 
-  @Column("varchar", {
-    name: "regional_head_name",
+  @Column('varchar', {
+    name: 'regional_head_name',
     nullable: true,
-    comment: "大区负责人名称",
+    comment: '大区负责人名称',
     length: 128,
   })
   regionalHeadName: string | null;
 
-  @Column("bigint", {
-    name: "provincial_head_id",
+  @Column('bigint', {
+    name: 'provincial_head_id',
     nullable: true,
-    comment: "省区负责人ID",
+    comment: '省区负责人ID',
   })
   provincialHeadId: string | null;
 
-  @Column("varchar", {
-    name: "provincial_head_name",
+  @Column('varchar', {
+    name: 'provincial_head_name',
     nullable: true,
-    comment: "省区负责人名称",
+    comment: '省区负责人名称',
     length: 128,
   })
   provincialHeadName: string | null;
 
-  @Column("varchar", {
-    name: "contact",
+  @Column('varchar', {
+    name: 'contact',
     nullable: true,
-    comment: "下单联系人",
+    comment: '下单联系人',
     length: 128,
   })
   contact: string | null;
 
-  @Column("varchar", {
-    name: "contact_phone",
+  @Column('varchar', {
+    name: 'contact_phone',
     nullable: true,
-    comment: "下单联系人电话",
+    comment: '下单联系人电话',
     length: 32,
   })
   contactPhone: string | null;
 
-  @Column("varchar", {
-    name: "receiver_name",
+  @Column('varchar', {
+    name: 'receiver_name',
     nullable: true,
-    comment: "收货人姓名",
+    comment: '收货人姓名',
     length: 128,
   })
   receiverName: string | null;
 
-  @Column("varchar", {
-    name: "receiver_phone",
+  @Column('varchar', {
+    name: 'receiver_phone',
     nullable: true,
-    comment: "收货人电话",
+    comment: '收货人电话',
     length: 32,
   })
   receiverPhone: string | null;
 
-  @Column("varchar", {
-    name: "receiver_province",
+  @Column('varchar', {
+    name: 'receiver_province',
     nullable: true,
-    comment: "收货省份",
+    comment: '收货省份',
     length: 64,
   })
   receiverProvince: string | null;
 
-  @Column("varchar", {
-    name: "receiver_city",
+  @Column('varchar', {
+    name: 'receiver_city',
     nullable: true,
-    comment: "收货城市",
+    comment: '收货城市',
     length: 64,
   })
   receiverCity: string | null;
 
-  @Column("varchar", {
-    name: "receiver_district",
+  @Column('varchar', {
+    name: 'receiver_district',
     nullable: true,
-    comment: "收货区/街道",
+    comment: '收货区/街道',
     length: 64,
   })
   receiverDistrict: string | null;
 
-  @Column("varchar", {
-    name: "receiver_address",
+  @Column('varchar', {
+    name: 'receiver_address',
     nullable: true,
-    comment: "收货人详细地址",
+    comment: '收货人详细地址',
     length: 200,
   })
   receiverAddress: string | null;
 
-  @Column("decimal", {
-    name: "amount",
-    comment: "订单总金额(发货金额），单位：元",
+  @Column('decimal', {
+    name: 'amount',
+    comment: '订单总金额(发货金额），单位：元',
     precision: 12,
     scale: 2,
   })
   amount: string;
 
-  @Column("decimal", {
-    name: "credit_amount",
-    comment: "额度计算总额，单位：元",
+  @Column('decimal', {
+    name: 'credit_amount',
+    comment: '额度计算总额，单位：元',
     precision: 12,
     scale: 2,
     default: () => "'0.00'",
   })
   creditAmount: string;
 
-  @Column("decimal", {
-    name: "replenish_amount",
-    comment: "产生的货补金额, 单位：元",
+  @Column('decimal', {
+    name: 'replenish_amount',
+    comment: '产生的货补金额, 单位：元',
     precision: 12,
     scale: 2,
     default: () => "'0.00'",
   })
   replenishAmount: string;
 
-  @Column("decimal", {
-    name: "auxiliary_sales_amount",
-    comment: "产生辅销金额, 单位：元",
+  @Column('decimal', {
+    name: 'auxiliary_sales_amount',
+    comment: '产生辅销金额, 单位：元',
     precision: 12,
     scale: 2,
     default: () => "'0.00'",
   })
   auxiliarySalesAmount: string;
 
-  @Column("decimal", {
-    name: "used_replenish_amount",
-    comment: "使用货补金额, 单位：元",
+  @Column('decimal', {
+    name: 'used_replenish_amount',
+    comment: '使用货补金额, 单位：元',
     precision: 12,
     scale: 2,
     default: () => "'0.00'",
   })
   usedReplenishAmount: string;
 
-  @Column("decimal", {
-    name: "used_replenish_ratio",
-    comment: "使用货补金额比例， 小数形式，如 0.10， 表示 10%",
+  @Column('decimal', {
+    name: 'used_replenish_ratio',
+    comment: '使用货补金额比例， 小数形式，如 0.10， 表示 10%',
     precision: 6,
-    scale: 4,
-    default: () => "'0.0000'",
+    scale: 3,
+    default: () => "'0.000'",
   })
   usedReplenishRatio: string;
 
-  @Column("decimal", {
-    name: "used_auxiliary_sales_amount",
-    comment: "使用辅销金额, 单位：元",
+  @Column('decimal', {
+    name: 'used_auxiliary_sales_amount',
+    comment: '使用辅销金额, 单位：元',
     precision: 12,
     scale: 2,
     default: () => "'0.00'",
   })
   usedAuxiliarySalesAmount: string;
 
-  @Column("decimal", {
-    name: "used_auxiliary_sales_ratio",
-    comment: "使用辅助销售金额比例， 同上",
+  @Column('decimal', {
+    name: 'used_auxiliary_sales_ratio',
+    comment: '使用辅助销售金额比例， 同上',
     precision: 6,
-    scale: 4,
-    default: () => "'0.0000'",
+    scale: 3,
+    default: () => "'0.000'",
   })
   usedAuxiliarySalesRatio: string;
 
-  @Column("int", {
-    name: "finished_product_box_count",
-    comment: "成品商品总箱数",
+  @Column('int', {
+    name: 'finished_product_box_count',
+    comment: '成品商品总箱数',
     default: () => "'0'",
   })
   finishedProductBoxCount: number;
 
-  @Column("int", {
-    name: "replenish_product_box_count",
-    comment: "货补商品总箱数",
+  @Column('int', {
+    name: 'replenish_product_box_count',
+    comment: '货补商品总箱数',
     default: () => "'0'",
   })
   replenishProductBoxCount: number;
 
-  @Column("int", {
-    name: "auxiliary_sales_product_box_count",
-    comment: "辅销商品总箱数",
+  @Column('int', {
+    name: 'auxiliary_sales_product_box_count',
+    comment: '辅销商品总箱数',
     default: () => "'0'",
   })
   auxiliarySalesProductBoxCount: number;
 
-  @Column("decimal", {
-    name: "paid_amount",
-    comment: "已付金额, 单位：元",
+  @Column('decimal', {
+    name: 'paid_amount',
+    comment: '已付金额, 单位：元',
     precision: 12,
     scale: 2,
     default: () => "'0.00'",
   })
   paidAmount: string;
 
-  @Column("decimal", {
-    name: "unpaid_amount",
-    comment: "未付金额, 单位：元",
+  @Column('decimal', {
+    name: 'unpaid_amount',
+    comment: '未付金额, 单位：元',
     precision: 12,
     scale: 2,
     default: () => "'0.00'",
   })
   unpaidAmount: string;
 
-  @Column("varchar", {
-    name: "remark",
+  @Column('varchar', {
+    name: 'remark',
     nullable: true,
-    comment: "备注信息",
+    comment: '备注信息',
     length: 1000,
   })
   remark: string | null;
 
-  @Column("varchar", {
-    name: "deleted",
-    comment: "是否删除，YES-删除，NO-未删除",
+  @Column('varchar', {
+    name: 'deleted',
+    comment: '是否删除，YES-删除，NO-未删除',
     length: 10,
     default: () => "'NO'",
   })
   deleted: string;
 
-  @Column("bigint", { name: "creator_id", comment: "创建人ID" })
+  @Column('bigint', { name: 'creator_id', comment: '创建人ID' })
   creatorId: string;
 
-  @Column("varchar", {
-    name: "creator_name",
-    comment: "创建人名字",
+  @Column('varchar', {
+    name: 'creator_name',
+    comment: '创建人名字',
     length: 255,
   })
   creatorName: string;
 
-  @Column("datetime", {
-    name: "created_time",
-    comment: "创建时间",
-    default: () => "CURRENT_TIMESTAMP",
+  @Column('datetime', {
+    name: 'created_time',
+    comment: '创建时间',
+    default: () => 'CURRENT_TIMESTAMP',
   })
   createdTime: Date;
 
-  @Column("bigint", { name: "reviser_id", nullable: true, comment: "修改人ID" })
+  @Column('bigint', { name: 'reviser_id', nullable: true, comment: '修改人ID' })
   reviserId: string | null;
 
-  @Column("datetime", {
-    name: "revised_time",
+  @Column('datetime', {
+    name: 'revised_time',
     nullable: true,
-    comment: "修改时间",
-    default: () => "CURRENT_TIMESTAMP",
+    comment: '修改时间',
+    default: () => 'CURRENT_TIMESTAMP',
   })
   revisedTime: Date | null;
 
-  @Column("varchar", {
-    name: "reviser_name",
+  @Column('varchar', {
+    name: 'reviser_name',
     nullable: true,
-    comment: "更新人名字",
+    comment: '更新人名字',
     length: 255,
   })
   reviserName: string | null;
 
-  @Column("varchar", {
-    name: "last_operate_program",
-    comment: "最后操作的程序",
+  @Column('varchar', {
+    name: 'last_operate_program',
+    comment: '最后操作的程序',
     length: 128,
   })
   lastOperateProgram: string;
